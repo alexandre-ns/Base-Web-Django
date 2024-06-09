4da74687e41a57275dffab26a158d84f1d1208c4
alexandre <alexandren49@gmail.com>
2024-06-09 20:41:43 -0300

    models post

--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Publication(models.Model):
+    title = models.CharField(max_length=200)  # Título da 
+    sub_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)  # Data e hora de criação
+
+    def __str__(self):
+        return self.title
+
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='publications/images/', blank=True, null=True)  # Imagem opcional
+    models.ForeignKey(Publication, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Text(models.Model):
+    name = models.CharField(max_length=150)
+    text = models.TextField()
+    models.ForeignKey(Publication, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
