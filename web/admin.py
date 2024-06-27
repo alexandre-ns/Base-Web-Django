2568c9f01dd4949f5e254e6a2787d5f29512fad0
alexandre <alexandren49@gmail.com>
2024-06-27 14:44:59 -0300

    new organization in the web admin file

--- web/admin.py
+++ web/admin.py
@@ -1,17 +1,24 @@
+"""admin of WEB SITE"""
 from django.contrib import admin
 from .models import Home, About, Contact
 
-# Remember that for the correct display on the site it is necessary that there is at least one page of each with the "active" True field
+# Remember that for the correct display on the site it is necessary
+# that there is at least one page of each with the "active" True field
 
 class HomeAdmin(admin.ModelAdmin):
+    """Registration and viewing of home page"""
+
     list_display = ('title', 'active')
 admin.site.register(Home, HomeAdmin)
 
 class AboutAdmin(admin.ModelAdmin):
+    """Registration and viewing of About page"""
+
     list_display = ('title', 'active')
 admin.site.register(About, AboutAdmin)
 
 class ContactAdmin(admin.ModelAdmin):
+    """Registration and viewing of Contact page"""
+
     list_display = ('title', 'active')
 admin.site.register(Contact, ContactAdmin)
-
