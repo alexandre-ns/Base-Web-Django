ea16505d3dee9f2898593a6ccf8c21b06194ef94
alexandre <alexandren49@gmail.com>
2024-06-27 14:29:14 -0300

    new organization in the publication admin file

--- publications/admin.py
+++ publications/admin.py
@@ -1,16 +1,24 @@
+"""admin of PUBLICAÇÕES"""
 from django.contrib import admin
 from .models import Publication, Text, Sources
 
 class TextAdmin(admin.TabularInline):
+    """Texts of publications"""
+
     model = Text
 admin.site.register(Text)
 
+
 class SourcesAdmin(admin.TabularInline):
+    """Sources of publications"""
+
     model = Sources
 admin.site.register(Sources)
 
-# TextAdmin and SourcesAdmin allow the creation of posts, text, and sources in one place
+
 class PublicationAdmin(admin.ModelAdmin):
+    """TextAdmin and SourcesAdmin allow the creation of posts, text, and sources in one place"""
+
     inlines = [TextAdmin, SourcesAdmin]
 
 admin.site.register(Publication, PublicationAdmin)
