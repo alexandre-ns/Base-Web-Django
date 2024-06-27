97b79c6c6dd265e1efcc6f69883e8a340136bfac
alexandre <alexandren49@gmail.com>
2024-06-27 13:06:06 -0300

    contact models

--- contact/models.py
+++ contact/models.py
@@ -1,27 +1,38 @@
+"""Module providing a function printing python version."""
 from django.db import models
 from django.core.validators import RegexValidator
 
-# Messages sent by website users
 class Message(models.Model):
+    """ Messages sent by website users"""
+
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
     )
     name = models.CharField("Nome", max_length=150)
     email_adress = models.EmailField("Email", max_length=150)
-    phone_number = models.CharField("Telefone", validators=[phone_regex], max_length=17, blank=True, null=True)
     message_text = models.TextField("Mensagem")
+    phone_number = models.CharField(
+        "Telefone", 
+        validators=[phone_regex],
+        max_length=17,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
     class Meta:
+        """Names"""
         verbose_name = "Mensagem"
         verbose_name_plural = "Mensagens"
 
 
-#social networks displayed on the site
+
 class SocialMedia(models.Model):
+    """ #social networks displayed on the site """
+
     TWITTER = "twitter"
     INSTAGRAM = "instagram"
     FACEBOOK = "facebook"
@@ -43,11 +54,13 @@ class SocialMedia(models.Model):
     )
     name = models.CharField("Nome identificador", max_length=150, blank=True, null=True)
     url = models.URLField("*URL", max_length=200, unique=True)
-    active = models.BooleanField("*Rede Social ativa", default=False) # Only the elements with the field active=True will be displayed on the site"
+    # Only the elements with the field active=True will be displayed on the site"
+    active = models.BooleanField("*Rede Social ativa", default=False)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
     class Meta:
+        """names"""
         verbose_name = "Rede Social"
         verbose_name_plural = "Redes Sociais"
